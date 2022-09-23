2abc6432f881c82ecffd6dca2a96fde43539c00c
Miguel de Icaza <miguel@gnome.org>
2022-09-23 17:20:39 -0400

    Update

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "CSSH",
-                      url: "https://github.com/migueldeicaza/Libssh2Prebuild/releases/download/Preview1.10.0+OpenSSL_1_1_1o_DEBUG.4/CSSH-Preview1.10.0+OpenSSL_1_1_1o_DEBUG.4.xcframework.zip",
+                      url: "https://github.com/migueldeicaza/Libssh2Prebuild/releases/download/CSSH-Preview1.10.0%2BOpenSSL_1_1_1o_DEBUG.4/CSSH-Preview1.10.0+OpenSSL_1_1_1o_DEBUG.4.xcframework.zip",
                       checksum: "1d2b2441bd32cfbf2255ca627062ad94f42efcb625c94d2d15b6094e55c3d687")
     ]
 )
