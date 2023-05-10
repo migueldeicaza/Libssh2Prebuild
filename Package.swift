977fe1a525b3d4dce5d1171ebc31fc4b616758d2
Miguel de Icaza <miguel@gnome.org>
2023-05-10 16:44:38 -0400

    Build 1.10.0+openssl-3.1.0_TRACK_2023_05_05_2.5

--- Package.swift
+++ Package.swift
@@ -10,6 +10,6 @@ let package = Package(
     targets: [
         .binaryTarget(name: "CSSH",
                       url: "https://github.com/migueldeicaza/Libssh2Prebuild/releases/download/1.10.0+openssl-3.1.0_TRACK_2023_05_05_2.5/CSSH-1.10.0+openssl-3.1.0_TRACK_2023_05_05_2.5.xcframework.zip",
-                      checksum: "841a1e68812ffcd53b943c2c49761df02097226fc01a28f784b0e3002d08e6ba")
+                      checksum: "f461c51829def755ce9d10c10a1eb3dfc6b2ee84d74b5dd11b007c808f86c3d3")
     ]
 )
