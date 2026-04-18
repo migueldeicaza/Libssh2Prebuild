c0b8fb311ed89a3697d1ce048566b73fda368025
Miguel de Icaza <miguel@gnome.org>
2026-04-18 17:21:50 -0400

    Bump to new pre-relase

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     ],
     targets: [
         .binaryTarget(name: "CSSH",
-                      url: "https://github.com/xibbon/Libssh2Prebuild/releases/download/1.11.2-dev.xibbon-2026-05-baseline%2Bg5a54b7c9eec7.openssl-3.5.6/CSSH-1.11.2-dev.xibbon-2026-05-baseline%2Bg5a54b7c9eec7.openssl-3.5.6.xcframework.zip",
+                      url: "https://github.com/migueldeicaza/Libssh2Prebuild/releases/download/1.11.2-dev-dev-xibbon-2026-05-baseline/CSSH-1.11.2-dev.xibbon-2026-05-baseline+g5a54b7c9eec7.openssl-3.5.6.xcframework.zip",
                       checksum: "35389b90a4729463cbd3bde7692156d027b0fde31579b4c0deb577bd9800a857")
     ]
 )
